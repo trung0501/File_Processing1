a3651a178cfc75f0ac112210f7a646e16e892175
trung0501 <nguyentrungt151@gmail.com>
2024-11-29 15:44:20 +0700

    flie

---
+++ Tuan7_1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Tuan7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSoNguyen = new System.Windows.Forms.TextBox();
+            this.txtSoThuc = new System.Windows.Forms.TextBox();
+            this.txtXau = new System.Windows.Forms.TextBox();
+            this.cboLogic = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số nguyên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số thực";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Logic";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Xâu";
+            // 
+            // txtSoNguyen
+            // 
+            this.txtSoNguyen.Location = new System.Drawing.Point(196, 36);
+            this.txtSoNguyen.Name = "txtSoNguyen";
+            this.txtSoNguyen.Size = new System.Drawing.Size(154, 22);
+            this.txtSoNguyen.TabIndex = 4;
+            // 
+            // txtSoThuc
+            // 
+            this.txtSoThuc.Location = new System.Drawing.Point(196, 90);
+            this.txtSoThuc.Name = "txtSoThuc";
+            this.txtSoThuc.Size = new System.Drawing.Size(154, 22);
+            this.txtSoThuc.TabIndex = 5;
+            // 
+            // txtXau
+            // 
+            this.txtXau.Location = new System.Drawing.Point(196, 191);
+            this.txtXau.Name = "txtXau";
+            this.txtXau.Size = new System.Drawing.Size(154, 22);
+            this.txtXau.TabIndex = 6;
+            // 
+            // cboLogic
+            // 
+            this.cboLogic.FormattingEnabled = true;
+            this.cboLogic.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.cboLogic.Location = new System.Drawing.Point(196, 135);
+            this.cboLogic.Name = "cboLogic";
+            this.cboLogic.Size = new System.Drawing.Size(154, 24);
+            this.cboLogic.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(196, 277);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(154, 36);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Lưu nội dung";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(413, 277);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(102, 36);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRead.Location = new System.Drawing.Point(590, 277);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(117, 36);
+            this.btnRead.TabIndex = 10;
+            this.btnRead.Text = "Đọc file";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 375);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cboLogic);
+            this.Controls.Add(this.txtXau);
+            this.Controls.Add(this.txtSoThuc);
+            this.Controls.Add(this.txtSoNguyen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSoNguyen;
+        private System.Windows.Forms.TextBox txtSoThuc;
+        private System.Windows.Forms.TextBox txtXau;
+        private System.Windows.Forms.ComboBox cboLogic;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ Tuan7_1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tuan7_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("data.txt"))
+            {
+                MessageBox.Show("Ghi dữ liệu thành công" +".");
+                return;
+            }
+
+            using (StreamReader reader = new StreamReader("data.txt"))
+            {
+                txtSoNguyen.Text = reader.ReadLine();
+                txtSoThuc.Text = reader.ReadLine();
+                cboLogic.SelectedItem = reader.ReadLine() == "True" ? "True" : "False";
+                txtXau.Text = reader.ReadLine();
+            }
+
+            MessageBox.Show("Đọc file thành công.");
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtSoNguyen.Clear();
+            txtSoThuc.Clear();
+            cboLogic.SelectedIndex = -1;
+            txtXau.Clear();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("data.txt"))
+            {
+                MessageBox.Show("File không tồn tại.");
+                return;
+            }
+
+            using (StreamReader reader = new StreamReader("data.txt"))
+            {
+                txtSoNguyen.Text = reader.ReadLine();
+                txtSoThuc.Text = reader.ReadLine();
+                cboLogic.SelectedItem = reader.ReadLine() == "True" ? "True" : "False";
+                txtXau.Text = reader.ReadLine();
+            }
+
+            MessageBox.Show("Đọc file thành công.");
+        }
+    }
+}
